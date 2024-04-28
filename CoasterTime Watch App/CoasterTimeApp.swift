fe21f861da7e729d70ebe12acef0b06b1623b11a
enzbdn <enzo.bodin@supdevinci-edu.fr>
2024-04-28 13:05:25 +0200

    Initial Commit

---
+++ CoasterTime Watch App/CoasterTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoasterTimeApp.swift
+//  CoasterTime Watch App
+//
+//  Created by Enzo Bodin on 28/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct CoasterTime_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
